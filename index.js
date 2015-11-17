5a38bbf253f8d505dc6572918ea470f811cee7b0
Luigi Poole <luigiplr@users.noreply.github.com>
2015-11-16 22:58:05 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Luigi POOLE wrote this. You can do whatever you want with this stuff.
+ * If we meet some day, and you think this stuff is worth it,
+ * you can buy me a beer in return.
+ * ----------------------------------------------------------------------------
+ */
+
 module.exports = require('./lib/sloth.js');
