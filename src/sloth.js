db65180ac7b2f8c9c3175c4145281261e7561328
Luigi Poole <luigipoole@outlook.com>
2015-11-27 22:07:56 -0800

    es6 sloth lib

---
+++ src/sloth.js
@@ -0,0 +1,95 @@
+import Slack from 'Slack';
+import _ from 'lodash';
+import {
+    parse as parseMsg
+}
+from 'parseMsg';
+import {
+    createInterface as readline
+}
+from 'readline';
+
+const rl = readline(process.stdin, process.stdout);
+const slackClient = new Slack(require('./../config.json').slackAPIToken, true, true);
+const config = require('./../config.json');
+
+
+slackClient.on('open', () => {
+    let unreads = slackClient.getUnreadCount();
+
+    console.log('Welcome to Slack. You are @', slack.self.name, 'of', slack.team.name);
+    return console.log('You have ', unreads, ' unread ', (unreads === 1) ? 'message' : 'messages');
+});
+
+
+slackClient.on('message', () => {
+    let user = slackClient.getUserByID(message.user);
+    let channel = slackClient.getChannelGroupOrDMByID(message.channel);
+    let text = message.text;
+
+    if (message.type === 'message' && (text !== null) && (channel !== null)) {
+        parseMsg(user, text, channel)
+            .then(response => {
+                switch (response.type) {
+
+                    case 'dm':
+                        slackClient.openDM(message.user, openDmData => {
+                            if (openDmData.ok) {
+
+                                response.forEach((responseline) => {
+                                    console.log('REPLY: ', responseline);
+                                    dmChannel.send(responseline);
+                                });
+
+                            }
+                        });
+                        break;
+                    case 'channel':
+                        responsefromparse.forEach(function(responseline) {
+                            console.log('REPLY: ', responseline);
+                            channel.send(responseline);
+                        });
+                        break:
+
+                    case 'remote-channel':
+
+
+                        break;
+                }
+
+            })
+            .catch(console.error);
+    }
+
+});
+
+slack.on('error', error => {
+    return console.error("Error: " + error);
+});
+
+slack.login();
+
+
+rl.on('line', cmd => {
+    switch (cmd.split(' ')[0]) {
+        case 'say':
+            var channel = _.filter(slack.channels, item => {
+                return item.name === cmd.split(' ')[1];
+            });
+            channel = slack.getChannelGroupOrDMByID(channel[0].id);
+            channel.send(cmd.split(' ').splice(2).join(' '));
+            break;
+        case 'quit':
+        case 'exit':
+            console.log('Killing Bot.');
+            process.exit();
+            break;
+        default:
+            console.log('You just typed: ' + cmd);
+    }
+});
+
+
+process.on('uncaughtException', err => {
+    console.log(err);
+});
