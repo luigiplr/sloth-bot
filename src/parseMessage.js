d67ce9c4a6b7ed6bda8c589c58d0b3b66b15ad72
Js41637 ftw <js41637@gmail.com>
2015-11-28 23:24:37 +1100

    holy shit

--- src/parseMessage.js
+++ src/parseMessage.js
@@ -21,15 +21,7 @@ findPlugins().forEach(plugin => {
 module.exports = {
     parse(user, channel, text) {
         let command = text.substr(1).split(' ')[0];
-        var context;
-        // APPARENTLY NO FUCKING TEXT HAS A TEXT LENGTH OF THREE, HOW COULD I BE SO STUPID TO ASSUME OTHERWISE
-        if (text.length > 3) {
-            console.log('TEXT LENGTH IS GREATER THAN 1');
-            context = text.substr(1).split(' ').splice(1).join(' ');
-        } else {
-            console.log('TEXT LENGTH IS NOT GREATER THAN 1');
-            context = undefined;
-        }
+        let context = text.substr(1).split(' ').splice(1)[0] ? text.substr(1).split(' ').splice(1).join(' ') : undefined;
         return new Promise((resolve, reject) => {
             let call = false;
             let plugin = _.find(plugins, plugin => {
