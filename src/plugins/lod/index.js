705ad5f88324172a623b4ad9ab8711596e821fc9
Js41637 ftw <js41637@gmail.com>
2015-11-29 11:18:00 +1100

    ಠ_ಠ

---
+++ src/plugins/lod/index.js
@@ -0,0 +1,20 @@
+import Promise from 'bluebird';
+
+module.exports = {
+    commands: [{
+        alias: ['lod'],
+        command: 'lod'
+    }],
+    help: [{
+        command: ['lod'],
+        usage: 'lod <person>'
+    }],
+    lod(user, channel, input = '') {
+        return new Promise((resolve, reject) => {
+            return resolve({
+                type: 'channel',
+                message: 'ಠ_ಠ ' + input
+            });
+        }) ;
+    }
+};
