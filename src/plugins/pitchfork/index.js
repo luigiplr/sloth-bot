57546623ef398103c25eeae8c0e1bcf7c0cf87d4
Js41637 ftw <js41637@gmail.com>
2015-11-29 12:15:08 +1100

    ?

--- src/plugins/pitchfork/index.js
+++ src/plugins/pitchfork/index.js
@@ -14,7 +14,7 @@ module.exports = {
             return resolve({
                 type: 'channel',
                 messages: [
-                    'ANGRY AT ' + input.toUpperCase() + ' WANT TO JOIN THE MOB? I\'VE GOT YOU COVERED! COME ON DOWN TO',
+                    'ANGRY AT ' + input.toUpperCase() + '? WANT TO JOIN THE MOB? I\'VE GOT YOU COVERED! COME ON DOWN TO',
                     '/r/pitchforkemporium WE GOT \'EM ALL! http://i.imgur.com/LGLPjWP.png#' + Math.floor(Math.random() * 1000)
                 ]
             });
