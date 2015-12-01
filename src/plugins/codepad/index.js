19f745ec296e585db47135e240837db42804c870
Js41637 ftw <js41637@gmail.com>
2015-12-01 21:52:42 +1100

    Add eval (codepad)

---
+++ src/plugins/codepad/index.js
@@ -0,0 +1,44 @@
+import _ from 'lodash'
+import Promise from 'bluebird';
+import codepad from 'codepad';
+
+var langs = ['C','C++','D','Haskell','Lua','OCaml','PHP','Perl','Python','Ruby','Scheme','Tcl'];
+
+module.exports = {
+    commands: [{
+        alias: ['eval'],
+        command: 'eval'
+    }],
+    help: [{
+        command: ['eval'],
+        usage: 'eval <language> <code>'
+    }],
+    eval(user, channel, input) {
+        return new Promise((resolve, reject) => {
+            try {
+                if (!input || !input.split('```')[1]) {
+                    return resolve({
+                        type: 'dm',
+                        message: 'Usage: eval <langage> <code> | Evals the code in the specified language, valid languages are: ' + langs.join(' ')
+                    });
+                }
+
+                let type = input.split(' ')[0];
+                let code = _.unescape(input.split('```')[1]);
+
+                codepad.eval(type, code, (err, out) => {
+                    if (err) {
+                        console.log("Error: ", err);
+                    } else {
+                        resolve({
+                            type: 'channel',
+                            message: 'Output: ```' + out.output + '```'
+                        });
+                    }
+                }, true);
+            } catch (e) {
+                reject(e);
+            }
+        });
+    }
+};
