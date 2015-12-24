79397662ff415d088e04d9ba50107624c784dad4
vankasteelj <vankasteelj@gmail.com>
2015-12-24 15:47:20 +0100

    bonjourmadame plugin

---
+++ src/plugins/bonjourmadame/index.js
@@ -0,0 +1,69 @@
+import MetaInspector from 'node-metainspector';
+import Promise from 'bluebird';
+import moment from 'moment';
+
+module.exports = {
+    commands: [{
+        alias: ['hellolady'],
+        command: 'bonjourmadame'
+    }],
+    help: [{
+        command: ['bonjourmadame'],
+        usage: 'bonjourmadame [void, \'today\', <MM.DD.YYYY>]'
+    }],
+    bonjourmadame(user, channel, input) {
+        return new Promise((resolve, reject) => {
+            var url;
+
+            if (input) {
+                if (input === 'today') {
+                    url = 'http://ditesbonjouralamadame.tumblr.com/';
+                } else if (input.match(/[0-9\.-]/g)) {
+                    var diff = moment(new Date(input.match(/[0-9\.-]/g).join(''))).diff(moment(), 'days');
+                    if (diff === 0) {
+                        url = 'http://ditesbonjouralamadame.tumblr.com/';
+                    } else if (diff < 0) {
+                        url = 'http://ditesbonjouralamadame.tumblr.com/page/' + (diff * - 1 + 1);
+                    } else {
+                        return resolve({
+                            type: 'dm',
+                            message: 'Given date must be in the past. Usage: bonjourmadame [void, \'today\', <MM.DD.YYYY>]'
+                        });
+                    }                
+                } else {
+                    return resolve({
+                        type: 'dm',
+                        message: 'Usage: bonjourmadame [void, \'today\', <MM.DD.YYYY>]'
+                    });
+                }
+            } else {
+                url = 'http://ditesbonjouralamadame.tumblr.com/random';
+            }
+
+            var client = new MetaInspector(url, { timeout: 5000 });
+
+            client.on('fetch', function() {
+                if (client.images && !client.images[0].match(/logo|avatar/i)) {
+                    return resolve({
+                        type: 'channel',
+                        message: client.images[0]
+                    });
+                } else {
+                    return resolve({
+                        type: 'channel',
+                        message: 'No picture found :('
+                    });
+                }
+            });
+            
+            client.on('error', function(err){
+                return resolve({
+                    type: 'channel',
+                    message: 'Oopsies, I am all broken :('
+                });
+            });
+
+            client.fetch();
+        });
+    }
+};
