e5612e00ad0e7fbb538e412937de2358d5978dab
Js41637 ftw <js41637@gmail.com>
2016-01-01 00:33:22 +1100

    whitespace

--- src/slack.js
+++ src/slack.js
@@ -3,12 +3,12 @@ import needle from 'needle';
 const config = require('../config.json');
 
 module.exports = {
-	sendMessage(channel, input) {
-		needle.post('https://magics.slack.com/api/chat.postMessage', {
+    sendMessage(channel, input) {
+        needle.post('https://magics.slack.com/api/chat.postMessage', {
             text: input,
             channel: channel,
             as_user: 'true',
             token: config.slackAPIToken
         });
-	}
+    }
 };
