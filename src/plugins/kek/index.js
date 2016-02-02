0ca8eeaa8fbad07349f4ece33081fecef0155ab5
Js41637 ftw <js41637@gmail.com>
2016-02-02 21:47:44 +1100

    this is going very well

--- src/plugins/kek/index.js
+++ src/plugins/kek/index.js
@@ -4,6 +4,7 @@ import needle from 'needle';
 const apiKey = require('./../../../config.json').googleToken;
 const subscriberUrl = 'https://www.googleapis.com/youtube/v3/channels?part=statistics&forUsername=TheFineBros&fields=items/statistics/subscriberCount&key=' + apiKey;
 const originalSubCount = 14080108;
+var lastCheck;
 
 module.exports = {
     commands: [{
@@ -22,8 +23,9 @@ module.exports = {
                     let newCount = originalSubCount - subCount;
                     resolve({
                         type: 'channel',
-                        message: '*TheFineFags have lost about ' + addStupidCommas(newCount) + ' subscribers since their fuckup*'
+                        message: 'TheFineFags have lost about *' + addStupidCommas(newCount) + '* subscribers in total since their fuckup' + (newCount > lastCheck ? (' and have lost *' + addStupidCommas((newCount - lastCheck)) + '* more subscribers since I last checked') : newCount < lastCheck ? (' but have gained *' + addStupidCommas((lastCheck - newCount)) + '* since I last checked') : '')
                     });
+                    lastCheck = newCount;
                 } else {
                     reject(err);
                 }
