35cb6ba6bd6cfb34a2417c802b16af615a51903e
Luigi Poole <luigipoole@outlook.com>
2016-04-19 00:42:52 -0700

    well thats a bit of shit.

---
+++ gulpfile.babel.js
@@ -0,0 +1,30 @@
+import gulp from 'gulp'
+import babel from 'gulp-babel'
+import rimraf from 'gulp-rimraf'
+import plumber from 'gulp-plumber'
+import server from 'gulp-develop-server'
+import runSequence from 'run-sequence'
+
+/* Build Tasks */
+
+gulp.task('build', () => gulp.src('src/**/*.js')
+  .pipe(plumber())
+  .pipe(babel())
+  .on('error', err => {
+    gutil.log(gutil.colors.red('[Code Compilation Error]'))
+    gutil.log(gutil.colors.red(err.message))
+  })
+  .pipe(gulp.dest('build')))
+
+gulp.task('clean-build', () => gulp.src('build', { read: false }).pipe(rimraf()))
+
+gulp.task('watch-build', () => gulp.watch('src/**/*.js', ['build', server.restart]))
+
+gulp.task('sloth:start', () => server.listen({ path: './build/sloth.js' }))
+
+
+/* Watch Tasks */
+
+gulp.task('start-dev', callback => runSequence('clean-build', 'build', 'watch-build', 'sloth:start', callback))
+
+gulp.task('start', callback => runSequence('clean-build', 'build', 'sloth:start', callback))
--- src/sloth.js
+++ src/sloth.js
@@ -4,119 +4,122 @@ import config from '../config.json'
 import { parse as parseMsg } from './parseMessage'
 
 if (!config.prefix || !config.slackToken || !config.slackAPIToken) {
-    console.error("Invalid config, please fill in the first 3 required config fields");
-    process.exit();
+  console.error("Invalid config, please fill in the first 3 required config fields")
+  process.exit()
 }
 
-
 class slackClient extends Slack {
-    constructor() {
-        super(config.slackAPIToken, true, true)
+  constructor() {
+    super(config.slackAPIToken, true, true)
 
-        this._registerEvents()
-        this.login()
-    }
-    _registerEvents() {
-        this.once('open', () => {
-            let unreads = slackClient.getUnreadCount();
+    this._registerEvents()
+    this.login()
+  }
 
-            config.teamName = slackClient.team.domain;
-            config.botname = slackClient.self.name;
-            config.botid = slackClient.self.id;
+  _registerEvents() {
+    this.once('open', () => {
+      let unreads = slackClient.getUnreadCount();
 
-            console.log('Welcome to Slack. You are @' + slackClient.self.name, 'of', slackClient.team.name);
-            console.log('You have', unreads, 'unread', (unreads === 1) ? 'message' : 'messages');
+      config.teamName = slackClient.team.domain;
+      config.botname = slackClient.self.name;
+      config.botid = slackClient.self.id;
+
+      console.log('Welcome to Slack. You are @' + slackClient.self.name, 'of', slackClient.team.name);
+      console.log('You have', unreads, 'unread', (unreads === 1) ? 'message' : 'messages');
+    })
+    this.on('message', ::this._onNewMessage)
+
+
+    this.on('close', err => {
+      sendErrorToDebugChannel('slackClientError', `Websocket Connection Terminated, Restarting - ${err}`)
+      setTimeout(() => process.exit(1), 500)
+    })
+
+    this.on('error', err => {
+      sendErrorToDebugChannel('slackClientError', err);
+      setTimeout(() => process.exit(1), 500)
+    })
+  }
+
+  _onNewMessage(message) {
+    const user = this.getUserByID(message.user)
+    let channel = this.getChannelGroupOrDMByID(message.channel)
+    const { text, ts } = message
+
+    if (message.type === 'message' && text && channel && !message.subtype) {
+      parseMsg(user, channel, text, ts)
+        .then(response => {
+          if (!response) return false;
+
+          if (!response.type == 'dm' || !response.type == 'channel') return console.error("Invalid message response type, must be channel or dm");
+
+          this._checkIfDM(response.type, response.user ? response.user.id : user)
+            .then(DM => {
+              if (DM) channel = DM;
+              console.log("OUT", channel.name + ':', (response.message ? response.message : response.messages))
+              if (response.message && response.message.attachments) {
+                channel.postMessage(this._getParams(response.message.msg, response.message.attachments))
+              } else if (!response.multiLine) {
+                if (response.message) channel.send(response.message)
+                else if (response.messages) channel.postMessage(this._getParams(response.messages.join('\n')))
+              } else {
+                if (response.messages && response.messages[1]) response.messages.forEach(message => channel.send(message))
+                else return console.error("Invalid Multiline format, your array must contain more than 1 message and use the 'messages' response type");
+              }
+            })
         })
-        this.on('message', ::this._onNewMessage)
-
-
-        this.on('close', err => {
-            sendErrorToDebugChannel('slackClientError', `Websocket Connection Terminated, Restarting - ${err}`)
-            setTimeout(() => process.exit(1), 500)
-        })
-
-        this.on('error', err => {
-            sendErrorToDebugChannel('slackClientError', err);
-            setTimeout(() => process.exit(1), 500)
+        .catch(err => {
+          if (!err) return
+          console.error(`parseMsg Error: ${err}`)
+          if (typeof err === 'string') channel.send(err)
+          else throw (err)
         })
     }
+  }
 
-    _onNewMessage(message) {
-        const user = this.getUserByID(message.user)
-        let channel = this.getChannelGroupOrDMByID(message.channel)
-        const { text, ts } = message
+  _sendErrorToDebugChannel(type, error) {
+    if (!config.debugChannel) return
 
-        if (message.type === 'message' && text && channel && !message.subtype) {
-            parseMsg(user, channel, text, ts)
-                .then(response => {
-                    if (!response) return false;
+    if (error && error.message && error.stack) {
+      console.error("Caught Error:", type, error.message, error.stack);
 
-                    if (!response.type == 'dm' || !response.type == 'channel') return console.error("Invalid message response type, must be channel or dm");
 
-                    this._checkIfDM(response.type, response.user ? response.user.id : user)
-                        .then(DM => {
-                            if (DM) channel = DM;
-                            console.log("OUT", channel.name + ':', (response.message ? response.message : response.messages))
-                            if (response.message && response.message.attachments) {
-                                channel.postMessage(this._getParams(response.message.msg, response.message.attachments))
-                            } else if (!response.multiLine) {
-                                if (response.message) channel.send(response.message)
-                                else if (response.messages) channel.postMessage(this._getParams(response.messages.join('\n')))
-                            } else {
-                                if (response.messages && response.messages[1]) response.messages.forEach(message => channel.send(message))
-                                else return console.error("Invalid Multiline format, your array must contain more than 1 message and use the 'messages' response type");
-                            }
-                        })
-                })
-                .catch(err => {
-                    if (!err) return
-                    console.error(`parseMsg Error: ${err}`)
-                    if (typeof err === 'string') channel.send(err)
-                    else throw (err)
-                })
-        }
+      let i = 0;
+      const message = 'Caught ' + type + ' ```' + error.message + '\n' + error.stack + '```';
 
-        _sendErrorToDebugChannel(type, error) {
-            if (error && error.message && error.stack) {
-                console.error("Caught Error:", type, error.message, error.stack);
-
-                if (!config.debugChannel)
-                    return;
-
-                let i = 0;
-                const message = 'Caught ' + type + ' ```' + error.message + '\n' + error.stack + '```';
-
-                if (i < 4) {
-                    i++
-                    slackTools.sendMessage(config.debugChannel, message)
-                    setTimeout(() => {
-                        if (i > 0) i--
-                    }, 3000)
-                } else {
-                    slackTools.sendMessage(config.debugChannel, "Warning! Error spam, stopping bot")
-                    process.exit()
-                }
-            } else {
-                console.error("Caught Error:", type, error);
-                if (config.debugChannel)
-                    slackTools.sendMessage(config.debugChannel, "ABNORMAL ERROR: Caught " + type + ' ```' + error + '```');
-            }
-
-        }
+      if (i < 4) {
+        i++
+        slackTools.sendMessage(config.debugChannel, message)
+        setTimeout(() => {
+          if (i > 0) i--
+        }, 3000)
+      } else {
+        slackTools.sendMessage(config.debugChannel, "Warning! Error spam, stopping bot")
+        process.exit()
+      }
+    } else {
+      console.error("Caught Error:", type, error);
+      if (config.debugChannel) slackTools.sendMessage(config.debugChannel, "ABNORMAL ERROR: Caught " + type + ' ```' + error + '```');
     }
+  }
 
-    _getParams = (text = '', attachments = null) => { text, attachments, as_user: true, token: config.slackAPIToken };
+  _getParams = (text = '', attachments = null) => ({ text, attachments, as_user: true, token: config.slackAPIToken })
 
-    _checkIfDM = (type, user) => new Promise(resolve => (type == 'dm') ? this.openDM(user, { channel } => resolve(this.getChannelGroupOrDMByID(channel.id))) : resolve(0));
+  _checkIfDM = (type, user) => new Promise(resolve => {
+    if (type == 'dm')
+      this.openDM(user, ({ channel }) => resolve(this.getChannelGroupOrDMByID(channel.id)))
+    else
+      resolve(0)
+  })
 }
 
 process.on('uncaughtException', err => {
-    slackClient._sendErrorToDebugChannel('uncaughtException', err)
-    setTimeout(() => process.exit(1), 500)
+  slackInstance._sendErrorToDebugChannel('uncaughtException', err)
+  setTimeout(() => process.exit(1), 500)
 })
 
-process.on('unhandledRejection', err => => slackClient._sendErrorToDebugChannel('unhandledRejection', err))
+process.on('unhandledRejection', err => slackInstance._sendErrorToDebugChannel('unhandledRejection', err))
 
-process.on('rejectionHandled', err => => slackClient._sendErrorToDebugChannel('handledRejection', err))
+process.on('rejectionHandled', err => slackInstance._sendErrorToDebugChannel('handledRejection', err))
 
-new slackClient()
+const slackInstance = new slackClient()
