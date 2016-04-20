730937b099487fda1415f24871016545948c0b9f
Js41637 ftw <js41637@gmail.com>
2016-04-20 17:14:41 +1000

    US pls

--- src/plugins/steam/utils/steam.js
+++ src/plugins/steam/utils/steam.js
@@ -12,7 +12,7 @@ const endpoints = {
   miniProfile: `http://steamcommunity.com/miniprofile/%q%`, // Unused
   gameSummary: `http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${token}&steamid=%q%&include_played_free_games=1`,
   appDetailsBasic: `http://store.steampowered.com/api/appdetails?appids=%q%&filters=basic`,
-  appDetails: `http://store.steampowered.com/api/appdetails?appids=%q%&filters=basic,price_overview,release_date,metacritic`,
+  appDetails: `http://store.steampowered.com/api/appdetails?appids=%q%&filters=basic,price_overview,release_date,metacritic&cc=us`,
   searchApps: `http://steamcommunity.com/actions/SearchApps/%q%`, // Unused
   numPlayers: `http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=%q%`,
   userBans: `http://api.steampowered.com/ISteamUser/GetPlayerBans/v0001/?key=${token}&steamids=%q%`,
