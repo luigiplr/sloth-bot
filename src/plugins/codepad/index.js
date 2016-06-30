d4b3a2e5ebe15b3a264ef739b12d4525307c78e4
Js41637 ftw <js41637@gmail.com>
2016-06-30 17:12:53 +1000

    there we go

--- src/plugins/codepad/index.js
+++ src/plugins/codepad/index.js
@@ -1,4 +1,5 @@
 import Promise from 'bluebird'
+import _ from 'lodash'
 import codepad from 'codepad'
 
 const langs = ['C', 'C++', 'D', 'Haskell', 'Lua', 'OCaml', 'PHP', 'Perl', 'Python', 'Ruby', 'Scheme', 'Tcl']
@@ -17,7 +18,7 @@ export function codep(user, channel, input) {
         message: 'Usage: eval <langage> <code> | Evals the code in the specified language, valid languages are: ' + langs.join(' ')
       })
     let type = input.split(' ')[0].split('\n')[0]
-    let code = input.split('```')[1]
+    let code = _.unescape(input.split('```')[1])
     let rejected = false
     let timeout = setTimeout(function() {
       rejected = true
@@ -26,7 +27,8 @@ export function codep(user, channel, input) {
     codepad.eval(type, code, (err, out) => {
       if (!rejected) {
         clearTimeout(timeout)
-        return resolve({ type: 'channel', message: !err ? 'Output: ```' + out.output + '```' : `Error: ${err}` })
+        if (out.output.length < 6000) return resolve({ type: 'channel', message: !err ? 'Output: ```' + out.output + '```' : `Error: ${err}` })
+        else return reject("Error, output is too large to post")
       }
     }, true)
   })
