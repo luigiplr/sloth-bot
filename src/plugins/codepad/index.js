0004d3806d2f8a55c8d3c8e5f10e3e543f270b79
Js41637 ftw <js41637@gmail.com>
2016-06-30 17:04:52 +1000

    dont look at this please

--- src/plugins/codepad/index.js
+++ src/plugins/codepad/index.js
@@ -1,5 +1,4 @@
 import Promise from 'bluebird'
-import _ from 'lodash'
 import codepad from 'codepad'
 
 const langs = ['C', 'C++', 'D', 'Haskell', 'Lua', 'OCaml', 'PHP', 'Perl', 'Python', 'Ruby', 'Scheme', 'Tcl']
@@ -18,7 +17,7 @@ export function codep(user, channel, input) {
         message: 'Usage: eval <langage> <code> | Evals the code in the specified language, valid languages are: ' + langs.join(' ')
       })
     let type = input.split(' ')[0].split('\n')[0]
-    let code = _.unescape(input.split('```')[1])
+    let code = input.split('```')[1]
     let rejected = false
     let timeout = setTimeout(function() {
       rejected = true
--- src/plugins/help/index.js
+++ src/plugins/help/index.js
@@ -1,6 +1,7 @@
 import Promise from 'bluebird'
 import _ from 'lodash'
 import config from '../../../config.json'
+import { sendMessage } from '../../slack'
 
 export const plugin_info = [{
   alias: ['help', 'h'],
@@ -8,34 +9,57 @@ export const plugin_info = [{
   usage: 'shows help for commands'
 }]
 
-export function help(user, channel, context, ts, plugins, userLevel) {
+export function help(user, channel, context = 0, ts, plugins, userLevel) {
   return new Promise(resolve => {
-    let helpList = ["```"],
-      commands = [],
-      aliases = []
+    let noAdmin = parseInt(context),
+      helpList = [
+        [],
+        []
+      ],
+      commands = [
+        [],
+        []
+      ],
+      aliases = [
+        [],
+        []
+      ] // stupid js beautify
 
     plugins.forEach(plugin => {
       if (plugin.plugin_info && Array.isArray(plugin.plugin_info)) {
         plugin.plugin_info.forEach(help => {
-          if (!help.alias || !help.usage || (help.userLevel && help.userLevel.indexOf(userLevel) === -1)) return
+          if (!help.alias || !help.usage || (help.userLevel && help.userLevel.indexOf(userLevel) === -1) || (help.userLevel && noAdmin)) return
 
           let cmdalias = '';
           help.alias.forEach(cmd => cmdalias += config.prefix + cmd + ' ')
-          aliases.push(cmdalias)
-          commands.push(`${cmdalias}%pad%| ${help.usage}`)
+          if (aliases[0].length < 45) {
+            aliases[0].push(cmdalias)
+            commands[0].push(`${cmdalias}%pad%| ${help.usage}`)
+          } else { // dirty cheat
+            aliases[1].push(cmdalias)
+            commands[1].push(`${cmdalias}%pad%| ${help.usage}`)
+          }
         });
       }
     });
-    let padding = aliases.sort((a, b) => {
-      return b.length - a.length
-    })[0].length + 1
 
-    helpList.push(commands.map(cmd => {
-      let initialSize = cmd.split('|')[0].length - 5 // the %pad%
-      return cmd.replace("%pad%", new Array(padding - initialSize).join(' '))
-    }).sort())
-    helpList.push("```");
+    commands.forEach((thing, i) => {
+      let padding = aliases[i].sort((a, b) => {
+        return b.length - a.length
+      })[0].length + 1
 
-    return resolve({ type: 'dm', messages: _.flatten(helpList) })
+      helpList[i].push('```')
+      helpList[i].push(commands[i].map(cmd => {
+        let initialSize = cmd.split('|')[0].length - 5 // the %pad%
+        return cmd.replace("%pad%", new Array(padding - initialSize).join(' '))
+      }).sort())
+      helpList[i].push("```")
+    })
+
+    helpList.forEach(helps => {
+      sendMessage('@' + user.name, _.flatten(helps).join('\n'))
+    })
+
+    return resolve()
   })
 }
