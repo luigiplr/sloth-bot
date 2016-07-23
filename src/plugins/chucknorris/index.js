48c6f8685bbc91c5cf0651c41d7c3ef38689d024
Js41637 ftw <js41637@gmail.com>
2016-07-23 21:24:14 +1000

    Actually fix chuck norris

--- src/plugins/chucknorris/index.js
+++ src/plugins/chucknorris/index.js
@@ -1,5 +1,6 @@
 import Promise from 'bluebird'
-import chuck from 'chuck-norris-api'
+import needle from 'needle'
+import { unescape } from 'lodash'
 
 export const plugin_info = [{
   alias: ['cn', 'chucknorris'],
@@ -7,18 +8,12 @@ export const plugin_info = [{
   usage: 'CHUCK THE NORRIS'
 }]
 
-export function chucknorris(user, channel, input) {
-  return new Promise(resolve => {
-    console.log(input)
-    if (input) {
-      let split = input.split(' ')
-      chuck.getRandom({ firstName: split[0], lastName: split[1] || ' ' }).then(joke => {
-        return resolve({ type: 'channel', message: joke.value.joke.replace(/ +(?= )/g, '') })
-      })
-    } else {
-      chuck.getRandom().then(joke => {
-        return resolve({ type: 'channel', message: joke.value.joke })
-      })
-    }
+export function chucknorris(user, channel, input = 'Chuck Norris') {
+  return new Promise((resolve, reject) => {
+    let split = input.split(' ')
+    needle.get(`http://api.icndb.com/jokes/random?firstName=${split[0]}&lastName${split[1] ? '=' + split[1] : ''}`, (err, resp, body) => {
+      if (!err && body && body.value) return resolve(unescape(body.value.joke.replace(/ +(?= )/g, '')))
+      else return reject("Error fetching ChuckNorris Joke")
+    })
   })
 }
