061a72e3b936a6510f1b0abaac5317a03245cbdc
Js41637 <js41637@gmail.com>
2017-01-22 16:12:46 +1100

    Add movie support

--- src/plugins/trakt/index.js
+++ src/plugins/trakt/index.js
@@ -2,7 +2,7 @@ import Promise from 'bluebird'
 import { filter, truncate, capitalize, floor } from 'lodash'
 import moment from 'moment'
 import config from '../../../config.json'
-import { getSerieDetails } from './utils/trakt'
+import { getSerieDetails, getMovieDetails } from './utils/trakt'
 
 if (!config.traktAPIKey) console.error("Error: Trakt Plugin requires traktAPIKey")
 
@@ -24,7 +24,9 @@ export function searchMovies(user, channel, input) {
     if (!config.traktAPIKey) return console.error("Error: Trakt Plugin requires traktAPIKey")
     if (!input) return resolve({ type: 'dm', message: 'Usage: movie <query> - Returns movie information for query' })
 
-    return reject("Not implemented")
+    getMovieDetails(input).then(movie => {
+      resolve({ type: 'channel', message: generateMovieResponse(movie) })
+    }).catch(reject)
   })
 }
 
@@ -48,26 +50,80 @@ const getImgUrl = id => {
   return config.traktImageProxy.replace('%s', id)
 }
 
-//const generateMovieResponse = (movieDetails => {})
+const generateMovieResponse = movie => {
+  if (!movie) return 'Error: Missing movie data while generating response'
+  if (movie.runtime) {
+    var time = moment.duration(movie.runtime, 'minutes')
+    var duration = type => time[type]() !== 0 ? `${time[type]()} ${type.slice(0, -1)}${(time[type]() > 1 ? 's' : '')}` : false
+    var runtime = ['hours', 'minutes'].map(duration).filter(Boolean).join(' and ')
+  }
+
+  let out = {
+    attachments: [{
+      "title": `${movie.title} (${movie.year || 'Unknown'})`,
+      "title_link": `https://trakt.tv/shows/${movie.ids.slug}`,
+      "fallback": `${movie.title} (${movie.year}) - https://trakt.tv/shows/${movie.ids.slug}`,
+      "image_url": getImgUrl(movie.ids.imdb),
+      "text": movie.tagline,
+      "mrkdwn_in": ["text", "pretext", "fields"],
+      "color": "#c61017"
+    }]
+  }
+
+  out.attachments[0].fields = filter([{
+    "title": "Overview",
+    "value": truncate(movie.overview, { length: 450 }) || null,
+    "short": false
+  }, {
+    "title": "Released",
+    "value": movie.released ? moment(movie.released).format('MMMM Do YYYY') : null,
+    "short": true
+  }, {
+    "title": "Runtime",
+    "value": movie.runtime ? runtime : null,
+    "short": true
+  }, {
+    "title": "Certification",
+    "value": movie.certification || null,
+    "short": true
+  }, {
+    "title": "Genres",
+    "value": movie.genres ? (movie.genres.slice(0, 3).map(g => capitalize(g)).join(', ')) : null,
+    "short": true
+  }, {
+    "title": "Rating",
+    "value": floor(movie.rating, 1) || null,
+    "short": true
+  }, {
+    "title": "Trailer",
+    "value": movie.trailer && movie.trailer.includes('youtube') ? movie.trailer.replace(/^(https?):\/\//, '') : null,
+    "short": true
+  }, {
+    "title": "Homepage",
+    "value": movie.homepage ? movie.homepage.replace(/^(https?):\/\//, '') : null,
+    "short": true
+  }])
+  return out
+}
 
 const generateShowResponse = serie => {
   if (!serie) return 'Error: Missing serie data while generating response'
   if (serie.seasons[0].number == 0) serie.seasons.shift() // Don't count specials season
   if (serie.seasons[serie.seasons.length-1].aired_episodes == 0) serie.seasons.pop() // Ignore last season if no aired episodes
   let out = {
-      attachments: [{
-        "title": `${serie.title} (${serie.year || 'Unknown'})`,
-        "title_link": `https://trakt.tv/shows/${serie.ids.slug}`,
-        "fallback": `${serie.title} (${serie.year})`,
-        "image_url": getImgUrl(serie.ids.imdb),
-        "mrkdwn_in": ["text", "pretext", "fields"],
-        "color": "#c61017"
-      }]
-    }
-    // Filter the shit to remove nulls
+    attachments: [{
+      "title": `${serie.title} (${serie.year || 'Unknown'})`,
+      "title_link": `https://trakt.tv/shows/${serie.ids.slug}`,
+      "fallback": `${serie.title} (${serie.year})`,
+      "image_url": getImgUrl(serie.ids.imdb),
+      "mrkdwn_in": ["text", "pretext", "fields"],
+      "color": "#c61017"
+    }]
+  }
+  // Filter the shit to remove nulls
   out.attachments[0].fields = filter([{
     "title": "Overview",
-    "value": truncate(serie.overview, { length: 400 }) || null,
+    "value": truncate(serie.overview, { length: 450 }) || null,
     "short": false
   }, {
     "title": "First Aired",
@@ -94,12 +150,12 @@ const generateShowResponse = serie => {
     "value": serie.network || null,
     "short": true
   }, {
-    "title": "Homepage",
-    "value": serie.homepage ? serie.homepage.replace(/^(https?):\/\//, '') : null,
+    "title": "Trailer",
+    "value": serie.trailer && serie.trailer.includes('youtube') ? serie.trailer.replace(/^(https?):\/\//, '') : null,
     "short": true
   }, {
-    "title": "Trailer",
-    "value": serie.trailer ? serie.trailer.replace(/^(https?):\/\//, '') : null,
+    "title": "Homepage",
+    "value": serie.homepage ? serie.homepage.replace(/^(https?):\/\//, '') : null,
     "short": true
   }], 'value')
   return out
--- src/plugins/trakt/utils/trakt.js
+++ src/plugins/trakt/utils/trakt.js
@@ -8,24 +8,30 @@ const options = { headers: { "trakt-api-key": token } }
 
 const endpoints = {
   serie: 'shows/%s?extended=full',
-  search: 'search/show?extended=full&query=%s',
-  season: 'shows/%s/seasons?extended=full'
+  search: 'search/%t?extended=full&query=%s',
+  season: 'shows/%s/seasons?extended=full',
+  movie: 'movies/%s?extended=full'
 }
 
-const getURL = (type, input) => {
-  return APIUrl + endpoints[type].replace('%s', input)
+const types = {
+  serie: 'show',
+  movie: 'movie'
 }
 
-const getShowWithSlugOrSearch = input => {
+const getURL = (what, input, type) => {
+  return APIUrl + endpoints[what].replace('%s', input).replace('%t', types[type])
+}
+
+const getShowORMovieWithSlughOrSearch = (input, type) => {
   return new Promise((resolve, reject) => {
     let isID = input.includes('-s')
     let name = isID ? input.replace('-s', '').trim() : input
-    needle.get(getURL(isID ? 'serie' : 'search', name), options, (err, resp, body) => {
+    needle.get(getURL(isID ? type : 'search', name, type), options, (err, resp, body) => {
       if (resp.statusCode == 404) return reject("No results found")
       if (err || !body) return reject("Error fetching data from Trakt.tv")
       if (isID) return resolve(body)
       if (!body.length) return reject("No results found")
-      resolve(body[0].show)
+      resolve(body[0][type == 'movie' ? 'movie' : 'show'])
     })
   })
 }
@@ -37,11 +43,15 @@ const getSeasonInfo = serie => new Promise((resolve, reject) => {
   })
 })
 
+export function getMovieDetails(input) {
+  return getShowORMovieWithSlughOrSearch(input, 'movie')
+          .then(Promise.resolve)
+          .catch(Promise.reject)
+}
+
 export function getSerieDetails(input) {
-  return new Promise((resolve, reject) => {
-    getShowWithSlugOrSearch(input)
-      .then(getSeasonInfo)
-      .then(resolve)
-      .catch(reject)
-  })
+  return getShowORMovieWithSlughOrSearch(input, 'serie')
+          .then(getSeasonInfo)
+          .then(Promise.resolve)
+          .catch(Promise.reject)
 }
