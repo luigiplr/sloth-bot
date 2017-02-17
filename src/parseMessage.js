179e7df477d0bebed516505afeac76471e9d4cbc
Js41637 <js41637@gmail.com>
2017-02-17 12:49:49 +1100

    Disable sed

--- src/parseMessage.js
+++ src/parseMessage.js
@@ -1,4 +1,4 @@
-import { find } from 'lodash'
+import { find, get } from 'lodash'
 import permissions from './permissions'
 import config from '../config.json'
 import { deleteMessage, getHistory, findUser } from './slack'
@@ -21,25 +21,30 @@ export function parse(user, channel, text, ts) {
       return resolve(false)
     }
 
-    if (text.startsWith('s/')) {
+    // Sedbot
+    if (!config.disableSed && text.startsWith('s/')) {
       var split = text.split('/')
       if (split.length >= 3) {
         var [, word, replacement, who] = split
         who = who ? findUser(who) : undefined
         getHistory(channel.id, 35).then(messages => {
+          // Finds latest message that isn't the current message, isn't a bot message and includes the text
+          // also if a user is specified, make sure the message is from said user
           var matchedMessage = find(messages, m => (
             (!who || (who && who.id == m.user)) &&
             !m.bot_id && m.user !== config.botid && m.ts != ts && m.text && m.text.includes(word)
           ))
           if (!matchedMessage) return reject("Found no matching word in recent messages")
           var rx = new RegExp(word, 'g')
-          var whoSaid = findUser(matchedMessage.user) || {}
+          var whoSaid = findUser(matchedMessage.user)
           var newMessage = matchedMessage.text.replace(rx, replacement)
-          return resolve({ type: 'channel', message: newMessage, options: {
-            as_user: false,
-            username: whoSaid.name,
-            icon_url: whoSaid.profile.image_72
-          }})
+          if (whoSaid && whoSaid.name) {
+            return resolve({ type: 'channel', message: newMessage, options: {
+              as_user: false,
+              username: whoSaid.name,
+              icon_url: get(whoSaid, 'profile.image_72', '')
+            }})
+          } else return resolve({ type: 'channel', message: newMessage })
         })
       }
       return
@@ -48,7 +53,7 @@ export function parse(user, channel, text, ts) {
     if (text.charAt(0) !== config.prefix) return resolve(false);
 
     console.log("IN", user.name + ':', text)
-      // If user is ignored and is not an admin (or if the user is the bot itself)
+    // If user is ignored and is not an admin (or if the user is the bot itself)
     if (((permissions.allIgnored.indexOf(username) > -1) && userLevel != 'superadmin') || user.name.toString().toLowerCase() === config.botname)
       return resolve(false)
 
