70856407cec6a706ab12af55c48da968d9af8216
Js41637 <js41637@gmail.com>
2017-04-19 19:41:32 +1000

    das better

--- src/plugins/minecraft/index.js
+++ src/plugins/minecraft/index.js
@@ -1,4 +1,4 @@
-const mineping = require('mineping')
+const mcping = require('mc-ping-updated');
 
 export const plugin_info = [{
   alias: ['mc', 'minecraft'],
@@ -9,13 +9,17 @@ export const plugin_info = [{
 export function mc(user, channel, input) {
   return new Promise((resolve, reject) => {
     if (!input) return resolve({ type: 'dm', message: 'Usage: mc <ip> [port] - returns info about a MC server' })
-    const split = input.split(' ')
-    
-    mineping(3, split[0], split[1] || '25565', (err, resp) => {
+    var split = input.split(' ')
+    split[0] = split[0].startsWith('<http:') ? split[0].split('|')[1].slice(0, -1) : split[0]
+    mcping(split[0], parseInt(split[1]) || 25565, (err, resp) => {
       if (err || !resp) return reject("Error fetching server info")
-      const players = (resp.players || []).map(player => player.name || "UNKNOWN")
-      if (!players.length) return resolve({ type: 'channel', message: 'No players are currently on the server' })
-      return resolve({ type: 'channel', message: `Current Players on server:\n- ${players.join('\n- ')}` })
+      if (!resp.players || !resp.players.online) return resolve({ type: 'channel', message: 'No players are currently on the server' })
+      var players = (resp.players || { sample: []}).sample.map(player => player.name.length ? player.name : undefined).filter(a => a ? true : false)
+      return resolve({ type: 'channel', messages: [
+        `*Current players on the server (${resp.players.online}/${resp.players.max}):*`,
+        players.length ? ` - ${players.join("\n - ")}` : "Unable to view players"
+      ]})
+      
     })
   })
 }
