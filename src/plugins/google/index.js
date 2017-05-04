2215dd92da206cce06cb1f7df014f807b0ce89ab
Js41637 <js41637@gmail.com>
2017-05-05 07:32:20 +1000

    had a luigi moment

--- src/plugins/google/index.js
+++ src/plugins/google/index.js
@@ -22,7 +22,7 @@ const ytOpts = {
   key: config.googleAPIKey,
   type: 'video',
   videoEmbeddable: true,
-  safeSearch: config.youtubeSafeSerch || 'none'
+  safeSearch: config.youtubeSafeSearch || 'none'
 }
 
 const baseURL = `https://www.googleapis.com/customsearch/v1?num=5&start=1&hl=en&safe=${config.googleSafeSearch || 'off'}&key=${config.googleAPIKey}`
