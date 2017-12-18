c5721216fa0d435950f60a181e473db80c976e25
Js41637 <Js41637@users.noreply.github.com>
2017-12-18 20:27:52 +1100

    more gifs

--- src/plugins/gif/index.js
+++ src/plugins/gif/index.js
@@ -14,17 +14,17 @@ export function gif(user, channel, input = false) {
     if (!input) return resolve({ type: 'dm', message: 'Usage: gif <query> - Returns top 4 results for query' })
 
     giphy.search({
-      q: input.replace(' ', '+'),
-      limit: 4,
+      q: input,
+      limit: 20,
       rating: config.giphyRating || 'r',
       fmt: 'json'
     }, (err, res) => {
-      if (res.pagination.count > 0)
-        return resolve({
-          type: 'channel',
-          message: res.data[Math.floor(Math.random() * res.pagination.count)].images.fixed_height.url
-        });
-      else return reject('No Gifs Found :(')
+      if (err || res.pagination.count === 0) return reject('No Gifs Found :(')
+
+      return resolve({
+        type: 'channel',
+        message: res.data[Math.floor(Math.random() * res.pagination.count)].images.fixed_height.url + `#${Math.floor(Math.random() * 1000)}`
+      })
     })
   })
 }
