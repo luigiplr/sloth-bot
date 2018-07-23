9409dc56f6603382422caf31961240011b231da5
Js41637 <Js41637@users.noreply.github.com>
2018-07-23 19:34:28 +1000

    Add option to show examples on ramda

--- src/plugins/ramda/index.js
+++ src/plugins/ramda/index.js
@@ -4,17 +4,22 @@ import Matcher from 'did-you-mean'
 export const plugin_info = [{
   alias: ['ramda'],
   command: 'ramda',
-  usage: 'ramda <command> - returns info for command'
+  usage: 'ramda <command> [--example] - returns info for command, optionally returns example'
 }]
 
 const matcher = new Matcher(Object.keys(ramdaFunctions).join(' '))
 matcher.ignoreCase()
 matcher.setThreshold(2)
 
+const exampleRx = / ?(--example|--ex)/
+
 export async function ramda(user, channel, input) {
-  if (!input) return { type: 'dm', message: 'Usage: ramda <command> | Searches for ramda function' }
+  if (!input) return { type: 'dm', message: 'Usage: ramda <command> [--example] - Searches for ramda function, include `--example` to return an example as well' }
 
   let method = input === '__' ? input : input.replace('R.', '')
+
+  const showExample = exampleRx.test(method)
+  method = method.replace(exampleRx, '').trim()
 
   if (!ramdaFunctions[method.toLowerCase()]) {
     const match = matcher.get(method)
@@ -27,9 +32,11 @@ export async function ramda(user, channel, input) {
     `*Command:* \`${cmd.command}\``,
     `*Category:* ${cmd.category}`,
     `*Since:* ${cmd.since}`,
+    cmd.see.length > 0 && `*Similar:* ${cmd.see.map(s => `\`${s}\``).join(', ')}`,
     `*Description:* ${cmd.description}`,
-    `http://ramdajs.com/docs/#${cmd.name}`
-  ]
+    `http://ramdajs.com/docs/#${cmd.name}`,
+    showExample && `*Example:* \`\`\`${cmd.example}\`\`\``
+  ].filter(Boolean)
 
   return { type: 'channel', messages: msg, options: { unfurl_links: false } }
 }
