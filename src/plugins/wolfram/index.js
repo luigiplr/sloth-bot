f0e32a8f993db0165329a3f8d2d582eca27e333e
Js41637 <Js41637@users.noreply.github.com>
2018-07-23 20:23:31 +1000

    Fix wolfram

--- src/plugins/wolfram/index.js
+++ src/plugins/wolfram/index.js
@@ -1,4 +1,6 @@
-import { query } from './utils/wolfram'
+import needle from 'needle'
+import config from '../../../config.json'
+import _ from 'lodash'
 
 export const plugin_info = [{
   alias: ['calc', 'wolfram'],
@@ -10,6 +12,42 @@ export function wolfram(user, channel, input) {
   return new Promise((resolve, reject) => {
     if (!input) return resolve({ type: 'dm', message: 'Usage: wolfram <query> - Computes <query> using Wolfram Alpha.' })
 
-    query(input).then(resp => resolve({ type: 'channel', message: `*Result*: ${resp}` })).catch(reject)
+    if (!config.wolframAPIKey) return reject("Error: WolframAlpha API Key required to use this function")
+
+    let url = `http://api.wolframalpha.com/v2/query?input=${input}&appid=${config.wolframAPIKey}`
+
+    needle.get(url, (err, resp, body) => {
+      if (err || !body) {
+        return reject('Error querying wolfram API')
+      }
+
+      if (_.get(body, 'attributes.sucess') === 'false' || _.get(body, 'attributes.error') === 'true') {
+        return reject('Error returned from Wolfram')
+      }
+
+      // require('fs').writeFileSync('./shit.json', JSON.stringify(body, null, 2))
+
+      const interpretationTitle = _.get(body, ['children', 0, 'attributes', 'title'])
+      const interpretation = _.find(_.get(body, ['children', 0, 'children', 0, 'children'], []), { name: 'plaintext' })
+
+      if (!interpretation || !interpretationTitle) {
+        return reject('Error trying to interpret question from Wolfram')
+      }
+
+      const resultTitle = _.get(body, ['children', 1, 'attributes', 'title'])
+      const result = _.find(_.get(body, ['children', 1, 'children', 0, 'children'], []), { name: 'plaintext' })
+
+      if (!resultTitle || !result) {
+        return reject('Error trying to interpret result from Wolfram')
+      }
+
+      return resolve({
+        type: 'channel',
+        message: [
+          `*In:* _${interpretationTitle}_ = ${interpretation.value}`,
+          `*Out:* _${resultTitle}_ = ${result.value}`
+        ].join('\n')
+      })
+    })
   })
 }
