92c171d00a745eac44dac006fcd3591dd91de0bf
Js41637 <js41637@gmail.com>
2019-08-24 13:25:59 +1000

    crap

--- src/plugins/json/index.js
+++ src/plugins/json/index.js
@@ -1,4 +1,6 @@
 import { deleteMessage } from '../../slack.js'
+import request from 'request'
+import config from '../../../config'
 
 export const plugin_info = [{
   alias: ['json'],
@@ -7,6 +9,7 @@ export const plugin_info = [{
 }]
 
 export async function parseJson(user, channel, input, ts) {
+  console.log(user)
   if (!input) {
     return { type: 'dm', message: 'Usage: json <input> - returns prettified json' }
   }
@@ -16,21 +19,49 @@ export async function parseJson(user, channel, input, ts) {
     rawJson = input.slice(3, -3).trim()
   }
 
-  if (!rawJson.match(/^[{[](.+)?[}\]]$/)) {
+  if (!rawJson.match(/^[{[]([\s\S]+)?[}\]]$/)) {
     throw 'Invalid json'
   }
 
   try {
     const json = JSON.parse(rawJson)
+    const prettyJson = JSON.stringify(json, null, 2)
 
-    // Delete original message
-    deleteMessage(channel.id, ts)
+    if (prettyJson.split('\n').length >= 45) {
+      await uploadSnippet(user, prettyJson, channel.id)
+      deleteMessage(channel.id, ts)
 
-    return {
-      type: 'channel',
-      message: `JSON Output: \`\`\`${JSON.stringify(json, null, 2)}\`\`\``
+      return
+    } else {
+      deleteMessage(channel.id, ts)
+
+      return {
+        type: 'channel',
+        message: `JSON Output: \`\`\`${prettyJson}\`\`\``
+      }
     }
   } catch (err) {
-    throw `Error: ${err.message}`
+    throw `Error: ${err ? err.message : 'Unknown'}`
   }
+}
+
+async function uploadSnippet(user, text, channelId) {
+  return new Promise((resolve, reject) => {
+    request.post('https://slack.com/api/files.upload', {
+      qs: {
+        token: config.slackBotToken,
+        channels: channelId,
+        filename: `JSON Snippet from ${user.real_name || user.name}`,
+        filetype: 'javascript',
+        content: text
+      },
+      json: true
+    }, (err, resp, body) => {
+      if (err || !body.ok) {
+        return reject()
+      }
+
+      return resolve()
+    })
+  })
 }
